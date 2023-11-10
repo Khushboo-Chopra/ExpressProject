d8df3f027981f626fec74c95b2981ca655a91ff5
Khushboo <loveyouresteem@gmail.com>
2023-11-10 21:39:09 +0530

    Express Project

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
+const path = require('path');
 const express = require('express');
 const app = express();
+
+//npm package -> view engine handle bars
+app.set('view engine','hbs');
+
+app.set('views',path.join(__dirname,'views'));
+
 const port = 3000;
-const friends = [
-    {
-        id: 0,
-        name: 'Albert Einstein'
-    },
-    {
-        id: 1,
-        name: 'Sir Issac Newton'
-    }
-];
 
-app.get('/', (req, res) => {
-    res.json(friends);
+
+//middleware
+app.use((req,res,next)=>{
+const start = Date.now();
+next();
+const delta = Date.now() - start;
+console.log(`${req.method} ${req.baseUrl} ${req.url} ${delta} ms`);
 });
 
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
+//static file middleware
+app.use('/site', express.static('public'));
 
-    if (friend) {
-        res.status(200).json(friend);
-    }
-    else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        })
-    }
+//json parsing middleware
+app.use(express.json());
+
+app.use('/',(req,res)=>{
+    res.render('index',{
+        title:'My friends are very clever',
+        caption: 'Let\'s go skiing!'
+    });
 });
 
-app.get('/messages', (req, res) => {
-    res.send('HEEEEEEYYYYYYYYYY.....');
-});
+//middleware for routes
+app.use('/friends',friendsRouter);
 
+//mount the router on /messages path
+app.use('/messages',messagesRouter);
 
 app.listen(port, () => {
     console.log(`server is listening on ${port}`);
